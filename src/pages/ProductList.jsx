003d7aa84e59b0404dfb2dd39aa73715625288c5
gustavowillig <gugawillig@hotmail.com>
2022-11-09 16:05:36 -0300

    Começando o projeto

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
 import { Link } from 'react-router-dom';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class ProductList extends Component {
   state = {
-    isEmpty: false,
+    isEmpty: true,
     categories: [],
+    inputSearch: '',
+    products: '',
+    isSearched: false,
   };
 
   componentDidMount() {
     this.loadCategories();
   }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleProductsExhibition = async () => {
+    const { inputSearch } = this.state;
+    const responseAPI = await getProductsFromCategoryAndQuery(null, inputSearch);
+    this.setState({
+      isEmpty: false,
+      products: responseAPI.results,
+      isSearched: true,
+    });
+  };
 
   loadCategories = () => {
     getCategories().then((data) => this.setState({
@@ -19,40 +39,67 @@ export default class ProductList extends Component {
   };
 
   render() {
-    const { isEmpty, categories } = this.state;
+    const { isEmpty, categories, products, isSearched, inputSearch } = this.state;
     return (
       <div>
         <Link to="/shopping-cart" data-testid="shopping-cart-button">
           Carrinho de Compra
         </Link>
-        <input type="text" placeholder="Digite aqui" />
+        <input
+          data-testid="query-input"
+          name="inputSearch"
+          value={ inputSearch }
+          type="text"
+          onChange={ this.handleChange }
+          placeholder="Digite aqui"
+        />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.handleProductsExhibition }
+        >
+          Salvar
+        </button>
+        {
+          categories.map(({ id, name }) => (
+            <label
+              htmlFor={ id }
+              key={ id }
+              data-testid="category"
+            >
+              <input
+                type="radio"
+                name="categorias"
+                id={ id }
+              />
+              { name }
+            </label>
+          ))
+        }
         {isEmpty
-          ? (
+          && (
             <p data-testid="home-initial-message">
               Digite algum termo de pesquisa ou escolha uma categoria.
             </p>
+          )}
+        {isSearched && ((products.length > 0 && typeof products !== 'string')
+          ? (
+            products.map(({ id, title, thumbnail, price }) => (
+              <div
+                key={ id }
+                data-testid="product"
+              >
+                <h3>{ title }</h3>
+                <img src={ thumbnail } alt={ title } />
+                <p>{ price }</p>
+              </div>
+            ))
           )
           : (
-            <div>
-              {
-                categories.map((categorie) => (
-                  <label
-                    htmlFor={ categorie.id }
-                    key={ categorie.id }
-                    data-testid="category"
-                  >
-                    <input
-                      type="radio"
-                      name="categorias"
-                      id={ categorie.id }
-                    />
-                    {categorie.name}
-                  </label>
-                ))
-              }
-            </div>
-
-          )}
+            <p>
+              Nenhum produto foi encontrado
+            </p>
+          ))}
       </div>
     );
   }
