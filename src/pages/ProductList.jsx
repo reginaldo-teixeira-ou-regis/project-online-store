05e486de9da0753e1f6871483e0e4a2bb5654a8e
Erick Lauretti <erick.nighyt@hotmail.com>
2022-11-09 17:22:02 -0300

    feat/loadCategories

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 export default class ProductList extends Component {
   state = {
-    isEmpty: true,
+    isEmpty: false,
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.loadCategories();
+  }
+
+  loadCategories = () => {
+    getCategories().then((data) => this.setState({
+      categories: data,
+    }));
   };
 
   render() {
-    const { isEmpty } = this.state;
+    const { isEmpty, categories } = this.state;
     return (
       <div>
         <input type="text" placeholder="Digite aqui" />
-        { isEmpty
+        {isEmpty
           ? (
             <p data-testid="home-initial-message">
               Digite algum termo de pesquisa ou escolha uma categoria.
             </p>
           )
-          : 'a' }
+          : (
+            <div>
+              {
+                categories.map((categorie) => (
+                  <label
+                    htmlFor={ categorie.id }
+                    key={ categorie.id }
+                    data-testid="category"
+                  >
+                    <input
+                      type="radio"
+                      name="categorias"
+                      id={ categorie.id }
+                    />
+                    {categorie.name}
+                  </label>
+                ))
+              }
+            </div>
+
+          )}
       </div>
     );
   }
