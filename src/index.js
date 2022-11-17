a32e102af62ea06beea653b03e44fc7ba951ae13
Willian M. Vargas <willianm.vargas@gmail.com>
2022-11-17 10:32:45 -0300

    added scss/sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
+import './styles/main.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
